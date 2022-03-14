832e7f14e3ac8f09b8a153934cb2f4a9fd8eb2cb
Szymon Prud <UL0255098@edu.uni.lodz.pl>
2022-03-14 10:43:48 +0100

    Hashtable - nie skonczony

---
+++ ASD2_PrudS_Hashtable_220307/ASD2_PrudS_Hashtable_220307/ASD2_PrudS_Hashtable_220307.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+// visualgo.net/en/hashtable - program do projektowania sobie takiej tabeli 
+
+class Hashtable
+{
+public:
+	long klucz;
+	string chain;
+	Hashtable(int a, string b) : klucz(a), chain(b)
+	{
+	}
+
+	friend ostream& operator<<(ostream& s, Hashtable& K);
+};
+
+
+ostream& operator<<(ostream& s, Hashtable& K)
+{
+	return s << K.klucz << ", " << K.chain << endl;
+}
+
+
+int main()
+{
+	string linia;
+	ifstream plik;
+	plik.open("hashtable.txt");
+	if (plik.good() == true)
+	{
+		while (!plik.eof())
+		{
+			getline(plik, linia);
+			cout << linia << endl;
+		}
+		plik.close();
+	}
+
+	Hashtable X(123, "Amiga");
+
+	cout << X;
+
+	int test = 0;
+
+	
+
+	return 0;
+}
+
